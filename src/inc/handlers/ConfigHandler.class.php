55b5cc57ab421a7fe12cf8b9669b3e52cb7dd592
Milz0 <70971571+Milz0@users.noreply.github.com>
2026-01-11 21:25:42 +0000

    Add object storage test and reset functionality to ConfigHandler

--- src/inc/handlers/ConfigHandler.class.php
+++ src/inc/handlers/ConfigHandler.class.php
@@ -4,7 +4,22 @@ class ConfigHandler implements Handler {
   public function __construct($configId = null) {
     //we need nothing to load
   }
-  
+
+  private function isAjaxRequest() {
+    return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+  }
+
+  private function sendJsonAndExit($arr, $httpCode = 200) {
+    if (!headers_sent()) {
+      http_response_code($httpCode);
+      header('Content-Type: application/json; charset=utf-8');
+      header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+      header('Pragma: no-cache');
+    }
+    echo json_encode($arr);
+    die();
+  }
+
   public function handle($action) {
     try {
       switch ($action) {
@@ -12,6 +27,30 @@ class ConfigHandler implements Handler {
           AccessControl::getInstance()->checkPermission(DConfigAction::UPDATE_CONFIG_PERM);
           ConfigUtils::updateConfig($_POST);
           UI::addMessage(UI::SUCCESS, "Config was updated!");
+          break;
+        case DConfigAction::TEST_OBJECT_STORAGE:
+          AccessControl::getInstance()->checkPermission(DConfigAction::TEST_OBJECT_STORAGE_PERM);
+          if ($this->isAjaxRequest()) {
+            $ret = ConfigUtils::testObjectStorage($_POST);
+            if (!is_array($ret)) {
+              $ret = ['ok' => true, 'steps' => []];
+            }
+            if (!isset($ret['ok'])) {
+              $ret['ok'] = true;
+            }
+            if (!isset($ret['steps'])) {
+              $ret['steps'] = [];
+            }
+            $this->sendJsonAndExit($ret, 200);
+          }
+
+          ConfigUtils::testObjectStorage($_POST);
+          UI::addMessage(UI::SUCCESS, "Object storage test succeeded. You can now save these settings.");
+          break;
+        case DConfigAction::RESET_OBJECT_STORAGE:
+          AccessControl::getInstance()->checkPermission(DConfigAction::RESET_OBJECT_STORAGE_PERM);
+          ConfigUtils::resetObjectStorageProfile();
+          UI::addMessage(UI::SUCCESS, "Object Storage profile was deleted (reset to defaults).");
           break;
         case DConfigAction::REBUILD_CACHE:
           AccessControl::getInstance()->checkPermission(DConfigAction::REBUILD_CACHE_PERM);
@@ -33,9 +72,23 @@ class ConfigHandler implements Handler {
       }
     }
     catch (HTException $e) {
+      if ($action === DConfigAction::TEST_OBJECT_STORAGE && $this->isAjaxRequest()) {
+        $this->sendJsonAndExit([
+          'ok' => false,
+          'error' => $e->getMessage(),
+          'steps' => []
+        ], 200);
+      }
       UI::addMessage(UI::ERROR, $e->getMessage());
     }
     catch (HTMessages $m) {
+      if ($action === DConfigAction::TEST_OBJECT_STORAGE && $this->isAjaxRequest()) {
+        $this->sendJsonAndExit([
+          'ok' => false,
+          'error' => strip_tags($m->getHTMLMessage()),
+          'steps' => []
+        ], 200);
+      }
       UI::addMessage(UI::ERROR, $m->getHTMLMessage());
     }
   }
